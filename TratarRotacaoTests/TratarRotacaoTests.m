d35dd039ea3669ea7eae484d164d0aafa720aa8c
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-24 10:53:42 -0300

    Initial Commit

---
+++ TratarRotacaoTests/TratarRotacaoTests.m
@@ -0,0 +1,32 @@
+//
+//  TratarRotacaoTests.m
+//  TratarRotacaoTests
+//
+//  Created by Rafael Brigagão Paulino on 24/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "TratarRotacaoTests.h"
+
+@implementation TratarRotacaoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TratarRotacaoTests");
+}
+
+@end
